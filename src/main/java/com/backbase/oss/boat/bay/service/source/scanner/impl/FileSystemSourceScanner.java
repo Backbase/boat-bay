b7b18f2707f2ba82a9e0cc5f991cce3a2e4c593e
Sophie Janssens <sophiej@backbase.com>
2021-01-07 08:04:31 +0000

    Source scanner (still in progress, having an issue saving products) with index change log added - added_indexing.xml

--- src/main/java/com/backbase/oss/boat/bay/service/source/scanner/impl/FileSystemSourceScanner.java
+++ src/main/java/com/backbase/oss/boat/bay/service/source/scanner/impl/FileSystemSourceScanner.java
@@ -1,38 +1,55 @@
 package com.backbase.oss.boat.bay.service.source.scanner.impl;
 
 import com.backbase.oss.boat.bay.domain.*;
+import com.backbase.oss.boat.bay.domain.Source;
+import com.backbase.oss.boat.bay.domain.SourcePath;
+import com.backbase.oss.boat.bay.domain.Spec;
 import com.backbase.oss.boat.bay.domain.enumeration.SourceType;
-import com.backbase.oss.boat.bay.service.export.impl.FileSystemExporter;
+import com.backbase.oss.boat.bay.repository.*;
 import com.backbase.oss.boat.bay.service.source.scanner.SpecSourceScanner;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
+import java.io.IOException;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.security.core.parameters.P;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
 
 /**
  * Walks over a directory structure and based on directory names and depths, import specs
  *
  */
 @Slf4j
+@RequiredArgsConstructor
 public class FileSystemSourceScanner implements SpecSourceScanner {
-
-
+    @Autowired
+    private final PortalRepository portalRepository;
+    @Autowired
+    private final ProductRepository productRepository;
+    @Autowired
+    private final CapabilityRepository capabilityRepository;
+    @Autowired
+    private final ServiceDefinitionRepository serviceDefinitionRepository;
+    @Autowired
+    private final SpecRepository specRepository;
     @Getter
     @Setter
     private Source source;
@@ -40,30 +57,36 @@ public class FileSystemSourceScanner implements SpecSourceScanner {
     private final ObjectMapper objectMapper;
 
 
-    public FileSystemSourceScanner() {
-
-        objectMapper = new ObjectMapper(new YAMLFactory());
+    public List<Spec> scan() {
+        //objectMapper = new ObjectMapper(new YAMLFactory());
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.registerModule(new Jdk8Module());
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-    }
-    public List<Spec> scan() {
 
-        Path scanPath = Path.of(source.getPath());
+       // Path scanPath = Path.of(source.getPath());
+        List<Spec> specs = new ArrayList<>();
+        for (SourcePath p : source.getSourcePaths()) {
+            Path scanPath = Path.of(p.getName());
+            try {
+            List<Portal> portalsScanned =  Files.walk(scanPath)
+                    .filter(Files::isRegularFile)
+                    .filter(path -> path.endsWith("portal.yaml"))
+                    .map(path -> mapPortal(scanPath, path))
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .collect(Collectors.toList());
+                specs.addAll( portalsScanned.stream().flatMap(portal -> portal.getProducts().stream())
+                    .flatMap(product -> product.getCapabilities().stream())
+                    .flatMap(capability -> capability.getServiceDefinitions().stream())
+                    .flatMap(serviceDefinition -> serviceDefinition.getSpecs().stream()).collect(Collectors.toList()));
 
-        try {
-            Files.walk(scanPath)
-                .filter(Files::isRegularFile)
-                .filter(path -> path.endsWith("portal.yaml"))
-                .map(path -> mapPortal(scanPath, path))
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .collect(Collectors.toList());
-        } catch (IOException e) {
-            e.printStackTrace();
+            } catch (IOException e) {
+                log.error("Failed to scan path: {}", scanPath, e);
+            }
         }
-        return Collections.emptyList();
+
+        return specs;
     }
 
     @NotNull
@@ -80,10 +103,12 @@ public class FileSystemSourceScanner implements SpecSourceScanner {
             return Optional.empty();
         }
         log.debug("Scanned spec {}", spec);
+        if (!specRepository.existsById(spec.getId()))
+            specRepository.save(spec);
         return Optional.of(spec);
     }
 
-    private Optional<ServiceDefinition> mapService(Path scanPath, Path path){
+    private Optional<ServiceDefinition> mapService(Path scanPath, Path path, Capability capability){
         File serviceFile = new File(path.toString());
 
         ServiceDefinition service= new ServiceDefinition();
@@ -91,6 +116,10 @@ public class FileSystemSourceScanner implements SpecSourceScanner {
         try{
             if (serviceFile.exists()) {
                 service = objectMapper.readValue(serviceFile, ServiceDefinition.class);
+                if (!serviceDefinitionRepository.existsById(service.getId())) {
+                    service.setCapability(capability);
+                    serviceDefinitionRepository.save(service);
+                }
                 service.setSpecs(Files.walk(path.getParent())
                     .filter(Files::isRegularFile)
                     .filter(capPath -> capPath.endsWith("spec.yaml"))
@@ -103,55 +132,73 @@ public class FileSystemSourceScanner implements SpecSourceScanner {
             return Optional.empty();
         }
         log.debug("Scanned service {}", service);
+            serviceDefinitionRepository.save(service);
         return Optional.of(service);
     }
 
-    private Optional<Capability> mapCapability(Path scanPath, Path path){
-        Capability capability = new Capability();
+    private Optional<Capability> mapCapability(Path scanPath, Path path, Product product){
+        Capability capability;
 
         File capabilityFile = new File(path.toString());
 
         try {
             if (capabilityFile.exists()) {
                 capability = objectMapper.readValue(capabilityFile, Capability.class);
+                if (!capabilityRepository.existsById(capability.getId()) && productRepository.existsById(product.getId())) {
+                    capability.setProduct(product);
+                    capabilityRepository.save(capability);
+                }
                 capability.setServiceDefinitions(
                     Files.walk(path.getParent())
                     .filter(Files::isRegularFile)
                     .filter(capPath -> capPath.endsWith("service.yaml"))
-                    .map(servicePath -> mapService(scanPath, servicePath))
+                    .map(servicePath -> mapService(scanPath, servicePath, capability))
                     .filter(Optional::isPresent)
                     .map(Optional::get)
                     .collect(Collectors.toSet()));
 
+                log.debug("Scanned capability {}", capability);
+                capabilityRepository.save(capability);
+
+                return Optional.of(capability);
             }
         }catch (IOException e){
             return Optional.empty();
         }
-        log.debug("Scanned capability {}", capability);
-        return Optional.of(capability);
+        return Optional.empty();
     }
 
-    private Optional<Product> mapProduct(Path scanPath, Path path ){
-        Product product = new Product();
-
-
+    private Optional<Product> mapProduct(Path scanPath, Path path , Portal portal){
+        Product product;
         try {
             if (path.getFileName().toString().equals("product.yaml")) {
-                product = objectMapper.readValue(new File(path.toString()), Product.class);
+                File productPath = new File(path.toString());
+                product = objectMapper.readValue(productPath,Product.class);
+
+                if(!productRepository.existsById(product.getId())){
+                    product.setPortal(portal);
+                    productRepository.save(product);
+                }
+
                 product.setCapabilities(
                     Files.walk(path.getParent())
                     .filter(Files::isRegularFile)
                     .filter(capPath -> capPath.endsWith("capability.yaml"))
-                    .map(capPath -> mapCapability(scanPath, capPath))
+                    .map(capPath -> mapCapability(scanPath, capPath, product))
                     .filter(Optional::isPresent)
                     .map(Optional::get)
                     .collect(Collectors.toSet()));
+
+                log.debug("Scanned product {}", product);
+                productRepository.save(product);
+                return Optional.of(product);
             }
+
         }catch (IOException e){
             return Optional.empty();
         }
-        log.debug("Scanned product {}", product);
-        return Optional.of(product);
+
+        return Optional.empty();
     }
 
     private Optional<Portal> mapPortal(Path scanPath, Path path){
@@ -159,10 +206,15 @@ public class FileSystemSourceScanner implements SpecSourceScanner {
         try {
              File portalFile = new File(path.toString());
              portal = objectMapper.readValue(portalFile,Portal.class);
+
+            if(!portalRepository.existsById(portal.getId())) {
+                portalRepository.save(portal);
+            }
+
              portal.setProducts(Files.walk(path.getParent())
                  .filter(Files::isRegularFile)
                  .filter(productPath -> productPath.endsWith("product.yaml"))
-                 .map(productPath -> mapProduct(scanPath, productPath))
+                 .map(productPath -> mapProduct(scanPath, productPath, portal))
                  .filter(Optional::isPresent)
                  .map(Optional::get)
                  .collect(Collectors.toSet()));
@@ -171,6 +223,7 @@ public class FileSystemSourceScanner implements SpecSourceScanner {
         }
 
         log.debug("scanned portal {}",portal);
+        portalRepository.save(portal);
         return Optional.of(portal);
     }
 
--- src/test/java/com/backbase/oss/boat/bay/service/source/SourceScannerTest.java
+++ src/test/java/com/backbase/oss/boat/bay/service/source/SourceScannerTest.java
@@ -1,17 +1,56 @@
 package com.backbase.oss.boat.bay.service.source;
 
+import com.backbase.oss.boat.ExportException;
+import com.backbase.oss.boat.bay.BoatBayApp;
+import com.backbase.oss.boat.bay.domain.Portal;
 import com.backbase.oss.boat.bay.domain.Source;
+import com.backbase.oss.boat.bay.domain.SourcePath;
+import com.backbase.oss.boat.bay.domain.Spec;
+import com.backbase.oss.boat.bay.repository.*;
+import com.backbase.oss.boat.bay.service.export.ExportInfo;
+import com.backbase.oss.boat.bay.service.export.ExportOptions;
+import com.backbase.oss.boat.bay.service.export.ExportType;
+import com.backbase.oss.boat.bay.service.export.impl.FileSystemExporter;
 import com.backbase.oss.boat.bay.service.source.scanner.impl.FileSystemSourceScanner;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
+
+@SpringBootTest(classes = BoatBayApp.class)
 public class SourceScannerTest {
+    @Autowired
+    PortalRepository portalRepository;
+
+    @Autowired
+    ProductRepository productRepository;
+    @Autowired
+    CapabilityRepository capabilityRepository;
+    @Autowired
+    ServiceDefinitionRepository serviceDefinitionRepository;
+    @Autowired
+    SpecRepository specRepository;
+
+    @Autowired
+    FileSystemExporter fileSystemExporter;
 
     @Test
-    public void fileSystemSourceScannerTest(){
-        FileSystemSourceScanner scanner =new FileSystemSourceScanner();
+    public void fileSystemSourceScannerTest() throws ExportException {
+        FileSystemSourceScanner scanner =new FileSystemSourceScanner(portalRepository,productRepository,capabilityRepository,serviceDefinitionRepository,specRepository,new ObjectMapper(new YAMLFactory()));
         Source source = new Source();
-        source.setPath("/Users/sophiej/Documents/Projects/opensauce/boat-bay/boat-bay-data/Artifactory");
+        source.addSourcePath(new SourcePath().name("/Users/sophiej/Documents/Projects/opensauce/boat-bay/boat-bay-data/Artifactory"));
         scanner.setSource(source);
-        scanner.scan();
+        List<Spec> specs = scanner.scan();
+        Portal portal = portalRepository.getOne(specs.get(0).getPortal().getId());
+
+        ExportInfo export = fileSystemExporter.export(new ExportOptions(portal, ExportType.FILE_SYSTEM, "/target/exporter"));
+
+        assertThat(export.toString().equals(""));
+
+
     }
 }
