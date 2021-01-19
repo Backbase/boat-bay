921412878ee0afd197e41ec6ff0495dcddb1a455
Sophie Janssens <sophiej@backbase.com>
2021-01-19 10:49:46 +0000

    added endpoint specific spec class

--- src/main/java/com/backbase/oss/boat/bay/web/utils/UploadRequestBody.java
+++ src/main/java/com/backbase/oss/boat/bay/web/utils/UploadRequestBody.java
@@ -5,13 +5,32 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Lob;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UploadRequestBody {
-    private List<Spec> specs;
+    private List<UploadSpec> specs;
     private String location;
     private String projectId, artifactId, version;
+
+    @Data
+    class UploadSpec{
+
+        @NotNull
+        private String filename;
+        @NotNull
+        private String openApi;
+        @NotNull
+        private String key;
+
+        @NotNull
+        private String name;
+
+
+    }
 }
