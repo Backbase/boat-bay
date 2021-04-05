de742d79f5a793d25b10989d566511a0239eb2fa
Bart Veenstra <bartv@backbase.com>
2021-04-05 17:41:43 +0200

    maven source scanner WIP

---
+++ boat-bay-server/src/backbase/java/com/backbase/oss/boat/bay/exceptions/InvalidSpecException.java
@@ -0,0 +1,7 @@
+package com.backbase.oss.boat.bay.exceptions;
+
+public class InvalidSpecException extends Exception {
+    public InvalidSpecException(String message) {
+        super(message);
+    }
+}
