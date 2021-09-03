1ae1b08facfc2a2630aeb8f8e5a67de73f503025
Dhananjay Singh <dhananjay@backbase.com>
2021-09-03 03:28:12 +0200

    foreign key fix

--- boat-bay-server/src/backbase/java/com/backbase/oss/boat/bay/repository/BoatServiceRepository.java
+++ boat-bay-server/src/backbase/java/com/backbase/oss/boat/bay/repository/BoatServiceRepository.java
@@ -4,13 +4,14 @@ import com.backbase.oss.boat.bay.domain.Capability;
 import com.backbase.oss.boat.bay.domain.Portal;
 import com.backbase.oss.boat.bay.domain.Product;
 import com.backbase.oss.boat.bay.domain.ServiceDefinition;
-
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface BoatServiceRepository extends ServiceDefinitionRepository {
     Optional<ServiceDefinition> findByCapabilityAndKey(Capability capability, String key);
+
+    Optional<ServiceDefinition> findByKey(String key);
 
     Page<ServiceDefinition> findByCapabilityProduct(Product product, Pageable pageable);
 
